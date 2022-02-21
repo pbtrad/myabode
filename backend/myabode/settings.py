7d1bda7f5701e3d3257ef1fbb8e88c2b62eb5abf
paul <paulbrowne2@gmail.com>
2022-02-21 22:20:02 +0000

    Added serializer for Contact with ModelSerializer

--- backend/myabode/settings.py
+++ backend/myabode/settings.py
@@ -86,14 +86,20 @@ WSGI_APPLICATION = 'myabode.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': ('PASSWORD'),
         'HOST': 'localhost',
         
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'paulbrowne2@gmail.com'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
