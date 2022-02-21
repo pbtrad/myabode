cb9553e4a8d14cd2daefd76c6e6b0678f87cf0ad
paul <paulbrowne2@gmail.com>
2022-02-21 06:08:31 +0000

    Created accounts app

--- backend/myabode/settings.py
+++ backend/myabode/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!)3h!2m##868l@tja58)jd9)a&!+u*#cxdiipajelqccl40#0z'
+SECRET_KEY = 'django-insecure-b^2d5+1#r(&so7xvi0r=0+yevduy0)#krvwz^j3g7w9oi#xu97'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
