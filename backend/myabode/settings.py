88d0e24bf6b793c38efc804ee3300e0604cfa370
paul <paulbrowne2@gmail.com>
2022-03-10 16:40:38 +0000

    Changed to dj_database_url for heroku postgres

--- backend/myabode/settings.py
+++ backend/myabode/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 import environ
 env = environ.Env()
@@ -83,16 +84,17 @@ WSGI_APPLICATION = 'myabode.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('NAME'),
-        'USER': 'postgres',
-        'PASSWORD': env('PASSWORD'),
-        'HOST': 'localhost',
-        
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
