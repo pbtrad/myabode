358e6a9372008da6ea4e569fc2c06c2e306b24fb
paul <paulbrowne2@gmail.com>
2022-03-10 16:55:12 +0000

    Updated allowed hosts in settings.py

--- backend/myabode/settings.py
+++ backend/myabode/settings.py
@@ -29,6 +29,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = ['my-abode-pbtrad.herokuapp.com', 'localhost', '127.0.0.1']
+
 ALLOWED_HOSTS = []
 
 
